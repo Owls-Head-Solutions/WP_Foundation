3901f518d161b51c82b3320f20608d4016840f0f
DylanC92 <DylanC@owlsheadsolutions.com>
2015-06-02 15:48:32 -0400

    Updated WP CLI Script
    Adds functionality:
    - Removes all default plugins
    - Installs commonly used plugins (More to be added)
    - Removes all default posts and pages
    - Creates a home page
    - Tells WordPress to use the new home page as the default page for the
    website
    - Changes permalinks to “Pretty Permalinks”

--- puphpet/files/exec-once/b_install_wp.sh
+++ puphpet/files/exec-once/b_install_wp.sh
@@ -8,7 +8,7 @@ WP_TITLE="WPFoundation"
 WP_USER="admin"
 WP_USER_PASS="admin"
 WP_USER_EMAIL="admin@admin.com"
-WP_IP="192.168.56.102"
+WP_IP="192.168.56.103"
 
 cd /tmp
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -19,3 +19,21 @@ wp core download --allow-root
 wp core config --allow-root --path='/var/www/html' --dbname=${DB_NAME} --dbuser=${DB_USER_NAME} --dbpass=${DB_PASS}
 wp core install --allow-root --url=http://${WP_IP} --title=${WP_TITLE} --admin_user=${WP_USER} --admin_password=${WP_USER_PASS} --admin_email=${WP_USER_EMAIL}
 rm index.html
+
+wp plugin delete --allow-root akismet hello
+wp plugin install --allow-root advanced-custom-fields --activate
+wp plugin install --allow-root wp-statistics --activate
+
+wp post delete --allow-root $(wp post list --allow-root --post_type='post' --format=ids)
+wp post delete --allow-root $(wp post list --allow-root --post_type='post' --post_status=trash --format=ids)
+
+wp post delete --allow-root $(wp post list --allow-root --post_type='page' --format=ids)
+wp post delete --allow-root $(wp post list --allow-root --post_type='page' --post_status=trash --format=ids)
+
+wp post create --allow-root --post_type=page --post_title='Home' --post_status=publish --post_author=$(wp user get --allow-root ${WP_USER} --field=ID --format=ids)
+
+wp option update --allow-root show_on_front 'page'
+wp option update --allow-root page_on_front $(wp post list --allow-root --post_type=page --post_status=publish --posts_per_page=1 --pagename=home --field=ID --format=ids)
+
+wp rewrite structure --allow-root '/%postname%/' --hard
+wp rewrite flush --allow-root --hard
